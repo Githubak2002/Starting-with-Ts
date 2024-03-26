d2ba12e74d0a9491e639b2e0a25ab4d143111255
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-03-27 01:58:27 +0530

    Starting with Ts

---
+++ typeAliases.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var arrr = ["ak", "ko", false];
+var fun = function (newUser) {
+    return ({ naam: "ak", age: 21, isAdult: true });
+};
+var x = fun({ naam: "ak", age: 21, isAdult: true });
+console.log("User = ", x);
---
+++ typeAliases.ts
@@ -0,0 +1,19 @@
+
+// ============ Type Aliases –  ============
+type user = {
+    naam:string,
+    age:number,
+    isAdult:boolean
+}
+
+type humHia = (string|boolean) [];
+let arrr:humHia = ["ak","ko",false]
+
+const fun = (newUser:user):user => {
+    return({naam:"ak",age:21,isAdult:true})
+}
+
+let x = fun({naam:"ak",age:21,isAdult:true});
+console.log("User = ",x);
+
+export{}
